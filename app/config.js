989176e6842e0d3bb9d21a3658bcd8fee4be8c4f
sonia.turcotte@gmail.com <sonia.turcotte@gmail.com>
2017-11-15 19:45:11 +0000

    Change service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Dart crossing charge',
+  serviceName: 'Lock and Weir fishing permit',
 
   // Default port that prototype runs on
   port: '3000',
